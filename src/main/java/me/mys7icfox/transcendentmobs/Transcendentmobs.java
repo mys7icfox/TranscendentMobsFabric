077a5ce927ed6729d9684a278384ac2692c63fd2
mys7icfox <mys7icfox@gmail.com>
2026-04-20 18:45:13 -0400

    Test

---
+++ src/main/java/me/mys7icfox/transcendentmobs/Transcendentmobs.java
@@ -0,0 +1,14 @@
+package me.mys7icfox.transcendentmobs;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Transcendentmobs implements ModInitializer {
+    public static final String MOD_ID = "transcendentmobs";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+    }
+}
